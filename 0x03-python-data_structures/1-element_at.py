a9fa0df3e0b3cfe2f5a2ea5044a1f95c551bc273
Joseph Oluwaymi <joejoeoyemi@gmail.com>
2023-06-23 05:56:35 -0700

    Completed task 0-13

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    if idx < 0 or idx > len(my_list):
+        return None
+    for i in my_list:
+        if i == my_list[idx]:
+            return(i)
