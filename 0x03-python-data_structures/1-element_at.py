c9f0009b134ff9d79a93dacc13513593756b5987
Joseph Oluwaymi <joejoeoyemi@gmail.com>
2023-07-07 02:01:23 -0700

    Fixed bugs

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx > (len(my_list) - 1):
         return None
-    for i in my_list:
-        if i == my_list[idx]:
-            return(i)
+    return(my_list[idx])
