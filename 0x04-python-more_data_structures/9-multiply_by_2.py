d5a2b81362b4b80231f6761ddb78a9475d8a4a46
Joseph Oluwayemi <joejoeoyemi@gmail.com>
2023-12-08 11:00:51 -0800

    PEP 8 Validations

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -3,5 +3,5 @@ def multiply_by_2(a_dictionary):
     """Returns a new dictionary with all values multiplied by 2"""
     new_dict = {}
     for i, j in a_dictionary.items():
-        new_dict[i] = j *2
+        new_dict[i] = j * 2
     return (new_dict)
